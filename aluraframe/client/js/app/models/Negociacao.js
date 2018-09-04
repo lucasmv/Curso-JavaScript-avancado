9b93916330eeaa12ab244bc548d19e51098b0896
Lucas Magno <lucas.mv.silva@gmail.com>
2018-09-04 15:10:43 -0300

    Capitulo 5

--- aluraframe/client/js/app/models/Negociacao.js
+++ aluraframe/client/js/app/models/Negociacao.js
@@ -10,15 +10,15 @@ class Negociacao {
         return this._quantidade * this._valor;
     }
 
-    get data(){
+    get data() {
         return new Date(this._data.getTime());
     }
 
-    get quantidade(){
+    get quantidade() {
         return this._quantidade;
     }
 
-    get valor(){
+    get valor() {
         return this._valor;
     }
 }
